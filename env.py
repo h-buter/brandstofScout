dfb4bfc50c28f7a0c83d38b8231c0fd951d18c4a
huub <112480089+h-buter@users.noreply.github.com>
2025-07-21 15:12:29 +0200

    moved logos to env.py and fixed shell logo

--- env.py
+++ env.py
@@ -1,2 +1,20 @@
 topCheapestStations = 3
-fuelTypesOfInterestHomeAssistant = {"EURO98", "EURO95", "AUTOGAS"} #Allowed values: EURO98, EURO95, AUTOGAS, CNG, DIESEL, DIESEL_SPECIAL
+fuelTypesOfInterestHomeAssistant = {"EURO98", "EURO95", "AUTOGAS"} #Allowed values: EURO98, EURO95, AUTOGAS, CNG, DIESEL, DIESEL_SPECIAL, and maybe others but these are the ones I have found in my area
+
+#Map of known station names to logo URLs
+envLogos = {
+    "Shell": "/local/fuelstations/logos/shell.png",
+    "Tinq": "/local/fuelstations/logos/tinq.png",
+    "Esso": "/local/fuelstations/logos/esso.png",
+    "Texaco": "/local/fuelstations/logos/texaco.png",
+    "Total": "/local/fuelstations/logos/total.png",
+    "Tamoil": "/local/fuelstations/logos/tamoil.png",
+    "Argos": "/local/fuelstations/logos/argos.png",
+    "GT24": "/local/fuelstations/logos/unknown.png",
+    "Ok": "/local/fuelstations/logos/ok.png",
+    "Tango": "/local/fuelstations/logos/tango.png",
+    "Stam": "/local/fuelstations/logos/unknown.png",
+    "BP": "/local/fuelstations/logos/bp.png",
+    "CNG": "/local/fuelstations/logos/unknown.png",
+    "Peut": "/local/fuelstations/logos/unknown.png"
+}
--- main.py
+++ main.py
@@ -22,23 +22,7 @@ HA_URL = secretHomeassistantUrl
 HA_TOKEN = secretHomeAssistentToken
 
 
-#Map of known station names to logo URLs
-STATION_LOGOS = {
-    "Shell": "/local/fuelstations/logos/esso.png",
-    "Tinq": "/local/fuelstations/logos/tinq.png",
-    "Esso": "/local/fuelstations/logos/esso.png",
-    "Texaco": "/local/fuelstations/logos/texaco.png",
-    "Total": "/local/fuelstations/logos/total.png",
-    "Tamoil": "/local/fuelstations/logos/tamoil.png",
-    "Argos": "/local/fuelstations/logos/argos.png",
-    "GT24": "/local/fuelstations/logos/unknown.png",
-    "Ok": "/local/fuelstations/logos/ok.png",
-    "Tango": "/local/fuelstations/logos/tango.png",
-    "Stam": "/local/fuelstations/logos/unknown.png",
-    "BP": "/local/fuelstations/logos/bp.png",
-    "CNG": "/local/fuelstations/logos/unknown.png",
-    "Peut": "/local/fuelstations/logos/unknown.png"
-}
+STATION_LOGOS = envLogos
 
 apiKeyAnwb = 'NOT-NEEDED'
 
